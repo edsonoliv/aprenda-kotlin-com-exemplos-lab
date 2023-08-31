b33b2d73e0e8a2813ff74e4f6cfd0c6bfa236cd5
Edson <142681514+edsonoliv@users.noreply.github.com>
2023-08-30 21:45:08 -0300

    Adicionei um método adicionarConteudo à classe Formacao para permitir a adição de conteúdos à formação. Isso melhora a modularidade do código e torna mais fácil a adição de novos conteúdos.
    Adicionei um método listarConteudos à classe Formacao para listar os conteúdos presentes na formação. Isso ajuda a exibir as informações de maneira organizada.
    
    Inicializei a lista de conteúdos diretamente no construtor da classe Formacao, para que ela possa ser opcionalmente passada como parâmetro, mas tenha uma lista vazia como padrão.
    
    No bloco main, adicionei conteúdos à formação Kotlin usando o novo método adicionarConteudo, e depois listeios usando o método listarConteudos.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import kotlin.collections.MutableList
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val nivel: Nivel, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} matriculado na formação $nome")
+        } else {
+            println("Usuário ${usuario.nome} já está matriculado na formação $nome")
+        }
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo '${conteudo.nome}' adicionado à formação $nome")
+    }
+
+    fun listarConteudos() {
+        println("Conteúdos da formação $nome:")
+        for (conteudo in conteudos) {
+            println("${conteudo.nome} - Duração: ${conteudo.duracao} minutos")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 45)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 60)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO)
+
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoKotlin.matricular(usuario1) // Tentativa de matricular usuário já matriculado
+
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+
+    formacaoKotlin.listarConteudos()
 }
